83b607527286a0db44dd31cbb65c356c968af198
Aniket <aniket@rainingclouds.com>
2015-01-05 12:12:20 +0530

    changed the form of the function

--- keyword_parser.go
+++ keyword_parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"bytes"
 	"log"
 	"strings"
 )
@@ -9,23 +10,26 @@ import (
 // Function is supposed to find out occurences of translated keywords, and later on replace them with original
 // english keywords. This should also be tunable for laungages. This one is using ruby as a target.
 
-func ParseKeywords(input string, keywords map[string]string) int {
+func ProcessKeywords(input string, keywords map[string]string) (int, string) {
 	var quotes int
 	var singleQuote int
 	var blockCommentStart int
 	var blockCommentEnd int
 	var keywordsFound int
+	var processedStringBuffer bytes.Buffer
+
 	LineCommentString := "#" //for ruby
 	lines := strings.Split(input, "\n")
 
 	for _, line := range lines {
 		if strings.HasPrefix(strings.Trim(line, " "), LineCommentString) {
 			log.Println("skip line because commented ", line)
+			processedStringBuffer.WriteString(line + "\n")
 			continue
 		}
 		log.Println("chekcing line ", line)
 		fields := strings.Fields(line)
-		for _, field := range fields {
+		for index, field := range fields {
 			quotes += strings.Count(field, "\"")
 			singleQuote += strings.Count(field, "'")
 			blockCommentStart += strings.Count(field, "/*")
@@ -36,12 +40,25 @@ func ParseKeywords(input string, keywords map[string]string) int {
 				//token is Ok to check for reserved word, as it is not comments or inside a string
 				if keyword := keywords[field]; keyword != "" {
 					log.Println("Keyword found ", field, " for ", keyword)
+					if index == (len(fields) - 1) {
+						processedStringBuffer.WriteString(keyword)
+					} else {
+						processedStringBuffer.WriteString(keyword + " ")
+					}
 					keywordsFound += 1
+					continue
 				}
 			} else {
+
 				log.Println("this word in in quotes, or comments")
 			}
+			if index == (len(fields) - 1) {
+				processedStringBuffer.WriteString(field)
+			} else {
+				processedStringBuffer.WriteString(field + " ")
+			}
 		}
+		processedStringBuffer.WriteString("\n")
 	}
-	return keywordsFound
+	return keywordsFound, processedStringBuffer.String()
 }
--- keyword_parser_test.go
+++ keyword_parser_test.go
@@ -2,16 +2,18 @@ package parser
 
 import (
 	"io/ioutil"
+	"log"
 	"testing"
 )
 
-func TestKeywordParser(t *testing.T) {
+func TestProcessKeywords(t *testing.T) {
 	bytes, err := ioutil.ReadFile("test.rb")
 	if err != nil {
 		t.Error("Error reading file test.rb ", err.Error())
 	}
-	keywordsFound := ParseKeywords(string(bytes), map[string]string{"यासाठी": "for"})
+	keywordsFound, processedString := ProcessKeywords(string(bytes), map[string]string{"यासाठी": "for"})
 	if keywordsFound != 1 {
 		t.Error("Keyword found is", keywordsFound, ", instead of 1")
 	}
+	log.Println(processedString)
 }
